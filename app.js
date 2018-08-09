118b383829b895e61b5f49fdc8ea90e39a9b0083
zahra-yousefi <z.yoosefi6@gmail.com>
2018-08-10 01:01:57 +0430

    add memory buttons

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 var input = document.getElementById('screan_input'),
-    flag, operator, operand1, operand2;
-
-var numberArray = [], operatorArray = [];
-
-
-
+    flag, operator, operand1, operand2, memory;
+var memoryReadButton = document.getElementById('memory_read');
+var memoryCleanButton = document.getElementById('memory_clean');
 
 function number(value) {
     if (flag) {
         input.value = '';
         flag = false;
     }
-    if (value == '.') {
+    if (value === '.') {
         if (input.value) {
             input.value += value;
         } else {
             input.value += '0' + value;
         }
+    } else if (input.value === '0') {
+        input.value = value;
     } else {
         input.value += value;
     }
@@ -30,7 +29,6 @@ function oparation(op) {
     operator = op;
     flag = true;
 }
-
 
 function equal() {
     operand2 = input.value;
@@ -58,11 +56,41 @@ function clean() {
     flag = false;
 }
 
-
 function point(value) {
     if (operand1) {
         input.value += value;
     } else {
         input.value += '0' + value;
     }
+}
+
+function memoryStore() {
+    store(+input.value);
+}
+
+function memoryRead() {
+    if (memory === undefined) return;
+    input.value = memory;
+}
+
+function memoryClean() {
+    if (memory === undefined) return;
+    memory = undefined;
+    memoryReadButton.className = 'disable-memory-button';
+    memoryCleanButton.className = 'disable-memory-button';
+
+}
+
+function memoryAdd() {
+    store((memory || 0) + (+input.value));
+}
+
+function memoryMinus() {
+    store((memory || 0) - (+input.value));
+}
+
+function store(value) {
+    memoryReadButton.className = 'memory-button';
+    memoryCleanButton.className = 'memory-button';
+    memory = value;
 }
